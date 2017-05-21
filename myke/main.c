2e58cc3f796b61e5d1007c776f1c61ede0251eec
DBU <doublebirdsu@gmail.com>
2017-05-21 10:32:29 +0800

    V0.0.1

---
+++ myke/main.c
@@ -0,0 +1,17 @@
+/*
+ * main.c
+ *
+ *  Created on: 2017年5月21日
+ *      Author: JYS1105
+ */
+
+
+#include "main.h"
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+int main(int argc, char * argv[])
+{
+	printf("Hello GitHub!");
+}
---
+++ myke/main.h
@@ -0,0 +1,20 @@
+/*
+ * main.h
+ *
+ *  Created on: 2017年5月21日
+ *      Author: JYS1105
+ */
+
+#ifndef MYLINUX_MYKE_MAIN_H_
+#define MYLINUX_MYKE_MAIN_H_
+#ifdef __cplusplus
+extern "C" {
+#endif
+
+
+
+#ifdef __cplusplus
+}
+#endif
+
+#endif /* MYLINUX_MYKE_MAIN_H_ */
